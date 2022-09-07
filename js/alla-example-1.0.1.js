822d31c77e1ad90db8c0c7ee5a39bcd88aa8065c
allaclass <83661635+allaclass@users.noreply.github.com>
2022-09-07 10:55:31 +0900

    중간커밋
    중간커밋

---
+++ js/alla-example-1.0.1.js
@@ -0,0 +1,14 @@
+// 제    목 : alla-example-1.0.1.js
+// 작 성 자 : 김현수
+// 작 성 일 : 2022.09.07
+
+// var array_example = [];
+var url_example = '';
+var data_example = '';
+
+url_example = document.getElementById('txt_example');
+
+function inputData(){
+    data_example = url_example.value;
+    console.log(data_example);
+}
